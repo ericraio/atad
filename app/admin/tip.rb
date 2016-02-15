21f64e084e95fcca5ed953e39a23745824b38546
Eric Raio <ericraio@gmail.com>
2016-02-14 23:49:38 -0800

    adding test email button to active admin

--- app/admin/tip.rb
+++ app/admin/tip.rb
@@ -1,14 +1,17 @@
 ActiveAdmin.register Tip do
 
   form do |f|
-    f.inputs do
-      f.input :subject
-      f.input :body, as: :html_editor
+    inputs do
+      input :subject
+      input :body, as: :html_editor
     end
 
-    f.button :save
+    actions
+  end
+
+  action_item :send_test_email, only: :edit do
+    link_to 'Send Test Email', '#'
   end
 
   permit_params :subject, :body
-
 end
