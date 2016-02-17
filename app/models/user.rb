83c39d74baec5cabb3f835410a26c620d330fa7e
Eric Raio <ericraio@gmail.com>
2016-02-16 23:26:19 -0800

    fixing issue where ref code wasn't generating

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  before_create :generate_ref_code
+  before_validation :generate_ref_code, on: :create
 
   validates_presence_of :ref_code
   validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
