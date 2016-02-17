7e3112f00aeff7e4f9a75da25b2738057148b57e
Eric Raio <ericraio@gmail.com>
2016-02-16 21:15:11 -0800

    adding email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
   before_save :generate_ref_code
 
   validates_presence_of :ref_code
+  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+
   def generate_ref_code
     self.ref_code = SecureRandom.hex
   end
