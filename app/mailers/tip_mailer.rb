a81c3e242a40f17f1971198bd96c399bfbe1e1aa
Eric Raio <ericraio@gmail.com>
2016-02-17 15:44:32 -0800

    optimizing tip mailer

--- app/mailers/tip_mailer.rb
+++ app/mailers/tip_mailer.rb
@@ -1,9 +1,8 @@
 class TipMailer < ApplicationMailer
   default from: '"Eric from Agile Tips" <tips@agiletipaday.com>'
 
-  def daily(user_id, tip_id)
-    @user = User.find(user_id)
-    @tip = Tip.find(tip_id)
+  def daily(user, tip)
+    @user, @tip = user, tip
     mail to: @user.email, subject: "[VIP Agile Tip] ★  #{@tip.subject}"
   end
 
