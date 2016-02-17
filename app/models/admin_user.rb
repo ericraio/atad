bbe27d483e261230bd0fd414b83a863fad24adf8
Eric Raio <ericraio@gmail.com>
2016-02-17 15:40:42 -0800

    adding first and last name to admin users

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -3,4 +3,6 @@ class AdminUser < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates_presence_of :first_name, :last_name
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
-AdminUser.create(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+AdminUser.create(email: 'admin@example.com', first_name: 'John', last_name: 'Smith', password: 'password', password_confirmation: 'password')
 User.create(email: 'ericraio@gmail.com')
 Tip.create(subject: 'How to avoid a stomach ache', body: "Don't eat yellow snow")
