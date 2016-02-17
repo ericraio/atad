7f191bbd64ec8b2b99c1c3ee9d330a093c0803fd
Eric Raio <ericraio@gmail.com>
2016-02-16 21:15:43 -0800

    setting up email token

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,29 @@ class User < ActiveRecord::Base
   def generate_ref_code
     self.ref_code = SecureRandom.hex
   end
+
+  # Access token for a user
+  def email_token
+    self.class.create_email_token(self.id)
+  end
+
+  # Verifier based on our application secret
+  def self.verifier
+    ActiveSupport::MessageVerifier.new(Rails.application.secrets.secret_key)
+  end
+
+  # Class method for token generation
+  def self.create_email_token(id)
+    verifier.generate(id)
+  end
+
+  # Get a user from a token
+  def self.read_email_token(signature)
+    return if signature.nil?
+    id = verifier.verify(signature)
+    User.find(id)
+  rescue ActiveSupport::MessageVerifier::InvalidSignature
+    nil
+  end
+
 end
