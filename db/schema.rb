34ca7a1a818543178aa40204b0c6d0c0d9ae816d
Eric Raio <ericraio@gmail.com>
2016-02-16 21:15:59 -0800

    adding ref code to schema list

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160217034123) do
+ActiveRecord::Schema.define(version: 20160217045102) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,6 +76,9 @@ ActiveRecord::Schema.define(version: 20160217034123) do
     t.datetime "updated_at",                    null: false
     t.boolean  "daily_emails",  default: true
     t.boolean  "weekly_emails", default: false
+    t.string   "ref_code"
   end
+
+  add_index "users", ["ref_code"], name: "index_users_on_ref_code", using: :btree
 
 end
