ba8f843dd65a4d34c7b2f929b076848f02ee182f
Eric Raio <ericraio@gmail.com>
2016-02-17 21:11:07 -0800

    adding gravatar url for admin users

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -5,4 +5,10 @@ class AdminUser < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   validates_presence_of :first_name, :last_name
+
+  def gravatar_url
+    gravatar_id = Digest::MD5.hexdigest(self.email.downcase)
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
+  end
+
 end
