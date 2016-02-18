d9a70b0044985ad31859aab95b815425460238e5
Eric Raio <ericraio@gmail.com>
2016-02-17 21:10:57 -0800

    adding creator lookup for tips

--- app/mailers/tip_mailer.rb
+++ app/mailers/tip_mailer.rb
@@ -1,13 +1,18 @@
 class TipMailer < ApplicationMailer
-  default from: '"Eric from Agile Tips" <tips@agiletipaday.com>'
+  default from: '"Agile Tips" <tips@agiletipaday.com>'
 
   def daily(user, tip)
     @user, @tip = user, tip
-    mail to: @user.email, subject: "[VIP Agile Tip] ★  #{@tip.subject}"
+    mail({
+      to: @user.email,
+      subject: "[VIP Agile Tip] ★  #{@tip.subject}",
+      from: "\"#{@tip.creator.first_name} from Agile Tips\" <tips@agiletipaday.com>"
+    })
   end
 
   def weekly(user_id)
     @user = User.find(user_id)
     mail to: @user.email
   end
+
 end
--- app/models/tip.rb
+++ app/models/tip.rb
@@ -10,4 +10,19 @@ class Tip < ActiveRecord::Base
   def admin_permalink
     admin_tip_path(self)
   end
+
+  def creator
+    return @creator if @creator.present?
+    version = self.versions.first
+    return if version.blank?
+    @creator = AdminUser.find(version.whodunnit)
+  end
+
+  def updated_by
+    return @updated_by if @updated_by.present?
+    version = self.versions.last
+    return unless version.blank?
+    @updated_by = AdminUser.find(version.whodunnit)
+  end
+
 end
