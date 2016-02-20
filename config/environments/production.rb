490ecb45ade7e7ffff005c1dccbfe3f922eb3a54
Eric Raio <ericraio@gmail.com>
2016-02-19 23:42:07 -0800

    prepending www to email host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
   config.action_mailer.delivery_method = :ses
-  host = 'agiletipaday.com'
+  host = 'www.agiletipaday.com'
   Rails.application.routes.default_url_options[:host] = host
   config.action_mailer.default_url_options = {
     host: host
