45c32b6edf4fb8cb2a7d4bcb9c4917b5aa6f272f
Eric Raio <ericraio@gmail.com>
2016-02-20 23:45:35 -0800

    fixing issue with welcome clipboard

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,11 +1,12 @@
 jQuery(function($) {
   var el = $('.click-to-copy')[0];
-  new Clipboard(el);
+  if (el) {
+    new Clipboard(el);
 
-  $('.btn-clipboard').mouseenter(function() {
-    $(this).addClass('btn-clipboard-hover');
-  }).mouseleave(function() {
-    $(this).removeClass('btn-clipboard-hover');
-  });
-
+    $('.btn-clipboard').mouseenter(function() {
+      $(this).addClass('btn-clipboard-hover');
+    }).mouseleave(function() {
+      $(this).removeClass('btn-clipboard-hover');
+    });
+  }
 });
